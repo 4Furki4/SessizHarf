ac443b69242d14bc4dc77b068a70452c81c2b921
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-16 22:00:54 +0300

    Algoritma sınıfında yan yana olan ünsüzleri kontrol edip true veya false cevapları sırasıyla bool dizide deoplayan YanYanaIkiUnsuzKontrolu metodu oluşturuldu.

--- Algoritma.cs
+++ Algoritma.cs
@@ -1,7 +1,55 @@
+using System;
 namespace SessizHarf
 {
     public class Algoritma
     {
-        
-    }
-}
+        public bool[] YanYanaIkiUnsuzKontrolu(string[] arr)
+        {
+            bool[] trueFalse= new bool[arr.Length];
+            string unluler="aeoöuüıi";
+            int[] yanYanaIkiSessiz=new int[arr.Length];
+            int sıra=0;
+            foreach (var item in arr)
+            {
+                int counter=0;
+                foreach (var harf in item)
+                {
+                    
+                    if (harf.ToString().Contains(unluler))
+                    {
+                        counter=0;
+                    }
+                    else
+                    {
+                        counter++;
+                        if (counter==2)
+                        {
+                            yanYanaIkiSessiz[sıra]++;
+                        }
+                    }
+                    sıra++;
+                    
+                }
+                
+            }
+            for (int i = 0; i < trueFalse.Length; i++)
+            {
+                if (yanYanaIkiSessiz[i]>=1)
+                {
+                    trueFalse[i]=true;
+                }else
+                {
+                    trueFalse[i]=false;
+                }
+            }
+            return trueFalse;
+        }
+        public void SonuclariEkranaYazdır(bool[] arr)
+        {
+            foreach (var item in arr)
+            {
+                System.Console.WriteLine(item+" ");
+            }
+        }
+    } 
+}//Counteer
